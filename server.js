31be4d31b3545ce058391f76d0a06f9e5feaf659
Daniel Stuart <stu21008@byui.edu>
2022-09-26 13:46:17 -0600

    message

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require("express");
 const port = 3000;
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 app.listen(port, async ()=>{
     console.log('listening on port'+port);
 });
@@ -11,3 +12,28 @@ app.get('/', (req,res)=>{
 app.use(bodyParser.json());
 
 
+
+
+app.post("/login", (req,res)=>{
+    const loginEmail = req.body.userName;
+    console.log(JSON.stringify(req.body));
+    console.log("loginEmail", loginEmail);
+    const loginPassword = req.body.password;
+    console.log("loginPassword", loginPassword);
+    // res.send("Who are you?");
+
+    if (loginEmail == "fakeacccount@fake.com" && loginPassword == "byu1stuDent!"){
+        const token = uuidv4();
+        res.send(token);
+    } else{
+        res.status(401);//unauthorized
+        res.send("Invalid user or password");
+    }
+
+
+
+
+
+
+
+})

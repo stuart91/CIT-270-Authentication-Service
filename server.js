39a59820d1d29c5c0b8081c7442d48c2ec731bfa
Daniel Stuart <stu21008@byui.edu>
2022-09-30 13:44:56 -0600

    9.30.2022

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
-const bodyParser = require('body-parser');
 const express = require("express");
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
 const port = 3000;
 const app = express();
-const { v4: uuidv4 } = require('uuid');
+const redis = require('redis');
+const md5 = require('md5');
+const {createClient} = require('redis');
+
+const redisClient = createClient(
+{
+    Url:'redis://default@localhost6379'
+}
+);
+
 app.listen(port, async ()=>{
-    console.log('listening on port'+port);
+    await redisClient.connect();
+    console.log('listening on port:' ,+port);
 });
+
+const validatePassword = async (request, response)=>{
+    //await redisClient.connect()://creating a TCP socket
+};
+
 app.get('/', (req,res)=>{
     res.send('Hello World!')
 });
 app.use(bodyParser.json());
 
 
-
-
+app.post ('/user', (req,res)=>{
+    const newUserRequestObject = req.body;
+    console.log('New User:',JSON.stringify(newUserRequestObject));
+    redisClient.hSet('users',rew.body.email,JSON.stringify(newUserRequestObject));
+    res.send('New user'+newUserRequestObject.email+' added');
+})
 app.post("/login", (req,res)=>{
     const loginEmail = req.body.userName;
     console.log(JSON.stringify(req.body));
@@ -29,11 +49,5 @@ app.post("/login", (req,res)=>{
         res.status(401);//unauthorized
         res.send("Invalid user or password");
     }
-
-
-
-
-
-
 
 })
--- signupuser.cmd
+++ signupuser.cmd
@@ -1,2 +1,2 @@
 
-curl -v -d "@newuser.json" -X POST -H "Content-Type:application/text" https://dev.stedi.me/user
+curl -v -d "@newuser.json" -X POST -H "Content-Type:application/json" http://localhost:3000/user
